c0b81e28024587d36b3165deb1ffa04b58630855
Stuart <maccalsa@googlemail.com>
2020-11-07 19:13:08 +0000

    Add @Data annotation

--- src/main/java/funtimes/henry/groceries/data/Discount.java
+++ src/main/java/funtimes/henry/groceries/data/Discount.java
@@ -1,10 +1,12 @@
 package funtimes.henry.groceries.data;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
 @Builder
 public class Discount {
 
