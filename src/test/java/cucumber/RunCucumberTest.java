5fc5b3b334eec057daaa8de92146dd089cd181f3
Stuart <maccalsa@googlemail.com>
2020-11-07 20:10:13 +0000

    cucumber dependencies

---
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber/results.html"})
+public class RunCucumberTest {
+}
